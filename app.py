f7a266e8a77a0d54c2bbd5c19610b738dc321955
Mayank Chouhan <mynkchn.pu@gmail.com>
2026-01-21 10:22:27 +0530

    Change CORS settings to allow all origins
    Updated CORS middleware to allow all origins.

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8082"],
+    allow_origins=["*"],  # Allows all origins
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
